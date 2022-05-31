e75ff2e2e91badd2e47373f1ece0a8d7416cd840
Naia Flores <naia.flores@alumni.mondragon.edu>
2022-05-31 10:34:58 +0200

    ej3

---
+++ ej3.R
@@ -0,0 +1,32 @@
+#3.
+cantante<-function(nombre){
+  library(spotidy)
+  library(dplyr)
+  SPOTIFY_CLIENT_ID <-"79ba4f46f96048509889804d6ae01f8c"
+  SPOTIFY_CLIENT_SECRET <- "840aa37dd39a4b01a3d388f4d61ec931"
+  my_token <- get_spotify_api_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+
+  id_artista<-search_artists(
+  "nombre",
+  output = c("tidy", "raw", "id"),
+  limit = 20,
+  offset = 0,
+  token = my_token
+)
+
+  ARTIST_2<-get_artist_top_tracks(
+  "id_artista",
+  country="ES",
+  output = c("tidy", "raw"),
+  limit = 20,
+  offset = 0,
+  token = my_token)
+  
+  TRACK<-artista%>%select(name)
+  POULARIDAD<-artista%>%select(popularity)
+
+
+  print(paste(" La canción", TRACK," del cantante", ARTIST_2, "tiene una popularidad de", POPULARIDAD,". Este artista se hizo muy famoso por colaborar con ",ARTIST_1,))
+}
+
+
